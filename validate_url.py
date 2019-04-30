816fdc2c5677e9e7f16413fa4065b8d7372bb547
Tabish <taabishm2@gmail.com>
2019-04-30 11:35:16 +0530

    Url Validation module added

---
+++ validate_url.py
@@ -0,0 +1,24 @@
+#URL validity checker using RE
+import re
+
+def validate_url(url):
+
+    #Checks with http/https at beginning
+    regex = re.compile(
+            r'(?:http|ftp)s?://' # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    #Checks without http/https at beginning
+    regex2 = re.compile(
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+            r'localhost|' #localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+            r'(?::\d+)?' # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return (re.match(regex, url) is not None or re.match(regex2, url) is not None)
+
