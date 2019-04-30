0557fca66e113c7af8e5fe96e261643da380194b
Tabish <taabishm2@gmail.com>
2019-04-30 18:46:48 +0530

    Shortlink generated from encoded digest

--- shorty.py
+++ shorty.py
@@ -1,12 +1,13 @@
 import hashlib
 import validate_url
+import arbitrary_encoder
 
 def url_hash(url):
     '''Obtain 160 bit hash of the input url using SHA1'''
 
     url = url.encode('utf-8')
     h = hashlib.sha1(url)
-    return h.digest()
+    return h.hexdigest()
 
 def url_encode(url):
     '''Main function that executes all required processes to generate shortlink'''
@@ -17,7 +18,17 @@ def url_encode(url):
         raise Exception('URL_Validity_Error:Invlaid URL entered')
 
     h = url_hash(url)
-    
-    
+    base = arbitrary_encoder.base_change(h,16,73) #160 bit Hex always generates 26 digit Base73 value    
+    return base
 
-    
+def pick_shortlink(base):
+    ''' Picks a non conflicting value of shortlink (6 characters) from the base (26 characters) characters'''
+
+    for i in range(0,20):
+        shortlink = base[i:i+6]
+        if True: break  #If shortlink not found in database or if found & link is same 
+    else:
+        raise Exception("Shortkey_space_exhausted:Modify the url to check other scopes")
+
+
+        
