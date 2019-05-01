cb85de563bf3cda6a33e51e1d85e1a68f4fa2107
Tabish <taabishm2@gmail.com>
2019-05-01 15:33:08 +0530

    Shortlink generation & storing

--- shorty.py
+++ shorty.py
@@ -1,6 +1,7 @@
 import hashlib
 import validate_url
 import arbitrary_encoder
+import db_upload
 
 def url_hash(url):
     '''Obtain 160 bit hash of the input url using SHA1'''
@@ -19,16 +20,10 @@ def url_encode(url):
 
     h = url_hash(url)
     base = arbitrary_encoder.base_change(h,16,73) #160 bit Hex always generates 26 digit Base73 value    
-    return base
-
-def pick_shortlink(base):
-    ''' Picks a non conflicting value of shortlink (6 characters) from the base (26 characters) characters'''
 
     for i in range(0,20):
         shortlink = base[i:i+6]
-        if True: return(shortlink)  #If shortlink not found in database or if found & link is same 
+        if db_upload.upload_link(shortlink,url):
+            return(shortlink)  #If shortlink not found in database or if found & link is same 
     else:
         raise Exception("Shortkey_space_exhausted:Modify the url to check other scopes")
-
-
-        
