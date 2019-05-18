34c083fdb565e037766a9b7f2f211f58bfe92d09
Tabish <taabishm2@gmail.com>
2019-05-19 01:10:54 +0530

    Flask functionality

--- shortyweb.py
+++ shortyweb.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import shorty
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/",methods=['POST','GET'])
 def main():
     return render_template('index.html')
+
+@app.route("/converted", methods=['POST','GET'])
+def convert():
+    inp = request.form
+    link = inp['inputlink']
+
+    if shorty.validate_url.validate_url(link):
+        rcode = shorty.url_encode(link)
+    else:
+        rcode = shorty.url_decode(link)
+
+    return render_template('converted.html',link=rcode)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
